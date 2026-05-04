0ab8e3ec748b562f7a073080e2c6d410a27d463a
dummy123king <dummy123king@gmail.com>
2026-05-04 23:51:49 +0530

    added comments

--- linux_system_programming/ipc/pipe_demo.c
+++ linux_system_programming/ipc/pipe_demo.c
@@ -1,22 +1,37 @@
+/**
+ * @file pipe_demo.c
+ * @brief Demonstrates basic data transfer through an unnamed pipe within a single process.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/fcntl.h>
 #include <string.h>
 
-int main(int atgc, char **argv)
+/**
+ * @brief Main function to create a pipe, write to it, and read from it.
+ *
+ * This program creates a simple pipe, writes a string to the write end,
+ * and then reads the string from the read end, printing it to standard output.
+ *
+ * @param argc Number of command-line arguments.
+ * @param argv Array of command-line arguments.
+ * @return 0 on successful execution.
+ */
+int main(int argc, char **argv)
 {
     int fd[2];
-    char writeBuf[] = "Hello from Write END";
-    char readBuf[100];
-    int noOfBytesRead = 0;
+    char write_buf[] = "Hello from Write END";
+    char read_buf[100];
+    int no_of_bytes_read = 0;
     if (pipe(fd) != 0)
     {
         printf("Unable to create pipe\n");
         exit(EXIT_FAILURE);
     }
     
-    if(write(fd[1], writeBuf, strlen(writeBuf)) == -1)
+    if(write(fd[1], write_buf, strlen(write_buf)) == -1)
     {
         printf("Unable to write into the pipe\n");
         exit(EXIT_FAILURE);
@@ -24,16 +39,21 @@ int main(int atgc, char **argv)
 
     sleep(1);
     
-    noOfBytesRead = read(fd[0], readBuf, strlen(writeBuf));
+    no_of_bytes_read = read(fd[0], read_buf, strlen(write_buf));
 
-    if( noOfBytesRead == -1)
+    if( no_of_bytes_read == -1)
     {
         printf("Unable to Read into the pipe\n");
         exit(EXIT_FAILURE);
     }
-    readBuf[noOfBytesRead] = '\0';
+    read_buf[no_of_bytes_read] = '\0';
 
-    printf("Read Buffer: %s\n", readBuf);
+    printf("Read Buffer: %s\n", read_buf);
 
     return 0;
 }
+
+/*
+Expected Output:
+Read Buffer: Hello from Write END
+*/
