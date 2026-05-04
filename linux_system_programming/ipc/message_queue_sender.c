ed68cd0cb3d938ddb2f29fc3bef49bad74fb28f5
dummy123king <dummy123king@gmail.com>
2026-05-04 23:49:02 +0530

    added comments

--- linux_system_programming/ipc/message_queue_sender.c
+++ linux_system_programming/ipc/message_queue_sender.c
@@ -1,3 +1,8 @@
+/**
+ * @file message_queue_sender.c
+ * @brief Demonstrates sending data to a System V message queue.
+ */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -8,22 +13,33 @@
 
 #define MAX_MESSAGE_SIZE 256
 
-struct messageQueueDemo
+/**
+ * @brief Structure for the message queue buffer.
+ */
+struct message_queue_demo
 {
-    long int messageType;
+    long int message_type;
     char messgage[MAX_MESSAGE_SIZE];
 };
 
+/**
+ * @brief Main function to send messages to the queue.
+ *
+ * Connects to or creates the message queue with key 1234 and continuously
+ * writes messages containing incrementing integers to it.
+ *
+ * @return 0 on successful execution.
+ */
 int main()
 {
-    key_t messageID;
-    struct messageQueueDemo message;
+    key_t message_id;
+    struct message_queue_demo message;
     
     char buff[100];
     int data = 0;
     
-    messageID = msgget(1234, IPC_CREAT|0777);
-    if (messageID == -1)
+    message_id = msgget(1234, IPC_CREAT|0777);
+    if (message_id == -1)
     {
         printf("Unable to create message Queue\n");
         exit(EXIT_FAILURE);
@@ -32,8 +48,8 @@ int main()
     while (true)
     {
         sprintf(message.messgage, "data = %d", data);
-        message.messageType = 1;
-        if(msgsnd(messageID, &message, MAX_MESSAGE_SIZE, 0) == -1)
+        message.message_type = 1;
+        if(msgsnd(message_id, &message, MAX_MESSAGE_SIZE, 0) == -1)
         {
             printf("Unable to send the data\n");
         }
@@ -43,3 +59,8 @@ int main()
     
     return 0;
 }
+
+/*
+Expected Output:
+(Program runs continuously without output on successful send)
+*/
