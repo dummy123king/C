311df3431a1a63df929cc11b35b161ef49c41678
dummy123king <dummy123king@gmail.com>
2026-05-04 23:47:09 +0530

    added comments

--- linux_system_programming/ipc/message_queue_receiver.c
+++ linux_system_programming/ipc/message_queue_receiver.c
@@ -1,3 +1,8 @@
+/**
+ * @file message_queue_receiver.c
+ * @brief Demonstrates receiving data from a System V message queue.
+ */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -5,21 +10,32 @@
 #include <sys/ipc.h>
 #include <stdbool.h>
 
-struct messageQueueDemo
+/**
+ * @brief Structure for the message queue buffer.
+ */
+struct message_queue_demo
 {
-    long int messageType;
+    long int message_type;
     char messgage[BUFSIZ];
 };
 
+/**
+ * @brief Main function to receive messages from the queue.
+ *
+ * Connects to the message queue with key 1234 and continuously reads
+ * messages from it, printing the received data to the standard output.
+ *
+ * @return 0 on successful execution.
+ */
 int main()
 {
-    key_t messageID;
-    struct messageQueueDemo message;
+    key_t message_id;
+    struct message_queue_demo message;
     char buff[100];
-    int long messageToReceive = 0;
+    int long message_to_receive = 0;
 
-    messageID = msgget(1234, IPC_CREAT | 0777);
-    if (messageID == -1)
+    message_id = msgget(1234, IPC_CREAT | 0777);
+    if (message_id == -1)
     {
         printf("Unable to create message Queue\n");
         exit(EXIT_FAILURE);
@@ -27,7 +43,7 @@ int main()
 
     while (true)
     {
-        if (msgrcv(messageID, &message, BUFSIZ, messageToReceive, 0) == -1)
+        if (msgrcv(message_id, &message, BUFSIZ, message_to_receive, 0) == -1)
         {
             printf("Unable to Read the data\n");
         }
@@ -35,7 +51,15 @@ int main()
         sleep(1);
     }
     
-    msgctl(messageID, IPC_RMID, 0);
+    msgctl(message_id, IPC_RMID, 0);
 
     return 0;
 }
+
+/*
+Expected Output:
+RxData = data = 0
+RxData = data = 1
+RxData = data = 2
+...
+*/
