4a07a70deab0e6faf93a31f2c70515301ba8a9ae
dummy123king <dummy123king@gmail.com>
2026-05-04 23:53:22 +0530

    added comments

--- linux_system_programming/process/alarm.c
+++ linux_system_programming/process/alarm.c
@@ -1,28 +1,48 @@
+/**
+ * @file alarm.c
+ * @brief Demonstrates the use of the alarm() system call and SIGALRM signal.
+ */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <errno.h>
 
-static void callBack(int sigNo)
+/**
+ * @brief Signal handler for SIGALRM.
+ *
+ * This function is called when a SIGALRM signal is received. It prints a
+ * message and sets another alarm for 2 seconds.
+ *
+ * @param sig_no The signal number that triggered the handler.
+ */
+static void call_back(int sig_no)
 {
     printf("Called Function\n");
-    alarm(2);   
+    alarm(2);
 }
 
-
+/**
+ * @brief Main function to set up and handle alarms.
+ *
+ * Sets the default signal handler for SIGALRM, which terminates the process.
+ * It then schedules an alarm for 1 second and enters an infinite loop.
+ *
+ * @return This function does not return.
+ */
 int main()
 {
-    // if (signal(SIGALRM, callBack) == SIG_ERR)
-    // {
-    //     perror("Error");
-    // }
     if (signal(SIGALRM, SIG_DFL) == SIG_ERR)
     {
         perror("Error");
     }
 
     alarm(1);
-
-    for (;;);    
+    for (;;);
 }
+
+/*
+Expected Output:
+Alarm clock
+*/
