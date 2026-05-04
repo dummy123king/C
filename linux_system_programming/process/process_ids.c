33dd2e23394b9c52cad2774167edad1c89464f68
dummy123king <dummy123king@gmail.com>
2026-05-04 23:58:28 +0530

    added comments

--- linux_system_programming/process/process_ids.c
+++ linux_system_programming/process/process_ids.c
@@ -1,10 +1,30 @@
+/**
+ * @file process_ids.c
+ * @brief Demonstrates retrieving the current process ID and parent process ID.
+ */
+
 #include <stdio.h>
 #include <unistd.h>
 
-
+/**
+ * @brief Main function to print process IDs.
+ *
+ * Uses getpid() to get the current process ID and getppid() to get the
+ * parent process ID, then prints them to standard output.
+ *
+ * @param argc Number of command-line arguments.
+ * @param argv Array of command-line arguments.
+ * @return 0 on successful execution.
+ */
 int main(int argc, char **argv)
 {
     printf("ProcessID        --> PID  = %d\n", getpid());
     printf("Parent ProcessID --> PPID = %d\n", getppid());
     return 0;
 }
+
+/*
+Expected Output (PIDs will vary):
+ProcessID        --> PID  = 12345
+Parent ProcessID --> PPID = 12344
+*/
