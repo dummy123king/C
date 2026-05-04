576f661a84eebce3c195c207eb6b344cd01229fb
dummy123king <dummy123king@gmail.com>
2026-05-04 22:44:13 +0530

    added comments

--- miscellaneous/fibonacci.c
+++ miscellaneous/fibonacci.c
@@ -1,5 +1,15 @@
+/**
+ * @file fibonacci.c
+ * @brief Demonstrates Fibonacci sequence generation using iteration and recursion.
+ */
+
 #include <stdio.h>
 
+/**
+ * @brief Prints the Fibonacci sequence up to a given number of terms using iteration.
+ *
+ * @param number The number of terms to print.
+ */
 void fibonacci_with_iteration(int number)
 {
     int n1 = 0, n2 = 1, fib = 0;
@@ -25,6 +35,12 @@ void fibonacci_with_iteration(int number)
     printf("\n\n");
 }
 
+/**
+ * @brief Calculates the nth Fibonacci number using recursion.
+ *
+ * @param number The index of the Fibonacci number to calculate (0-based).
+ * @return The nth Fibonacci number, or -1 if the input is negative.
+ */
 int fibonacci_with_recursion(int number)
 {
     if (number < 0)
@@ -39,6 +55,11 @@ int fibonacci_with_recursion(int number)
     return (fibonacci_with_recursion(number - 1) + fibonacci_with_recursion(number - 2));
 }
 
+/**
+ * @brief Entry point of the program.
+ *
+ * @return 0 on successful execution.
+ */
 int main(void)
 {
     fibonacci_with_iteration(1);
@@ -52,3 +73,10 @@ int main(void)
     return 0;
 }
 
+/*
+Expected Output:
+0, 
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 
+
+*/
