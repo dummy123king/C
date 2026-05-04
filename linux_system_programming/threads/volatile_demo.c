4c265c314069e6611852ab791780e20c288a9e08
dummy123king <dummy123king@gmail.com>
2026-05-05 00:23:43 +0530

    added comments

--- linux_system_programming/threads/volatile_demo.c
+++ linux_system_programming/threads/volatile_demo.c
@@ -1,3 +1,8 @@
+/**
+ * @file volatile_demo.c
+ * @brief Demonstrates the use of the volatile keyword in multithreading.
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -6,9 +11,16 @@
 #include <semaphore.h>
 #include <time.h>
 
+/** @brief Shared variable to communicate between threads. */
 int volatile shared_data = 0;
 
-void *Task1(void *params)
+/**
+ * @brief Thread function that waits for the shared variable to change.
+ *
+ * @param params Unused parameter.
+ * @return NULL.
+ */
+void *task_1(void *params)
 {
     printf("Thread - 1 \n");
     
@@ -20,39 +32,52 @@ void *Task1(void *params)
         }
     }
     printf("Thread - 1 exited\n");
+    return NULL;
 }
 
-void *Task2(void *params)
+/**
+ * @brief Thread function that modifies the shared variable after a delay.
+ *
+ * @param params Unused parameter.
+ * @return NULL.
+ */
+void *task_2(void *params)
 {
     printf("Thread - 2 \n");
     sleep(5);
     shared_data = 5;
     printf("Thread - 2 exited\n");
+    return NULL;
  }
 
+/**
+ * @brief Main function to initialize threads and demonstrate volatile usage.
+ *
+ * @return 0 on successful execution, -1 on failure.
+ */
 int main()
 {
-    pthread_t hadle1, hadle2;
+    pthread_t handle_1, handle_2;
 
-    if (pthread_create(&hadle1, NULL, &Task1, NULL) != 0)
+    if (pthread_create(&handle_1, NULL, &task_1, NULL) != 0)
     {
         printf("Thread Creation Failed, File: %s Func: %s Line: %d\n", __FILE__, __func__, __LINE__);
         return -1;
     }
 
-    if (pthread_create(&hadle2, NULL, &Task2, NULL) != 0)
+    if (pthread_create(&handle_2, NULL, &task_2, NULL) != 0)
     {
         printf("Thread Creation Failed, File: %s Func: %s Line: %d\n", __FILE__, __func__, __LINE__);
         return -1;
     }
 
-    if (pthread_join(hadle1, NULL) != 0)
+    if (pthread_join(handle_1, NULL) != 0)
     {
         printf("Thread Join Failed, File: %s Func: %s Line: %d\n", __FILE__, __func__, __LINE__);
         return -1;
     }
 
-    if (pthread_join(hadle2, NULL) != 0)
+    if (pthread_join(handle_2, NULL) != 0)
     {
         printf("Thread Join Failed, File: %s Func: %s Line: %d\n", __FILE__, __func__, __LINE__);
         return -1;
@@ -60,3 +85,11 @@ int main()
 
     return 0;
 }
+
+/*
+Expected Output:
+Thread - 1 
+Thread - 2 
+Thread - 2 exited
+Thread - 1 exited
+*/
