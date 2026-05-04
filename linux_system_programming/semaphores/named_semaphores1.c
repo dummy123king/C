85ac36739dc3c90e0f0db9cebaf1212a86ff6f4c
dummy123king <dummy123king@gmail.com>
2026-05-05 00:09:25 +0530

    added comments

--- linux_system_programming/semaphores/named_semaphores1.c
+++ linux_system_programming/semaphores/named_semaphores1.c
@@ -1,9 +1,22 @@
+/**
+ * @file named_semaphores1.c
+ * @brief Opens and interacts with an existing named semaphore in Linux.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <semaphore.h>
 #include <fcntl.h>
 
+/**
+ * @brief Main function to access an existing named semaphore.
+ *
+ * Opens the named semaphore "semaphore", waits on it, enters a critical
+ * section, and then posts to it.
+ *
+ * @return 0 on successful execution.
+ */
 int main()
 {
     sem_t *semaphore;
@@ -12,27 +25,27 @@ int main()
     semaphore = sem_open("semaphore", O_RDWR);
     if (semaphore == SEM_FAILED)
     {
-        printf("Unbale to create semaphore exiting the program\n");
+        printf("Unable to open semaphore exiting the program\n");
         exit(EXIT_FAILURE);
     }
 
     if (sem_getvalue(semaphore, &value) == -1)
     {
-        printf("Unbale to Get semaphore value exiting the program\n");
+        printf("Unable to get semaphore value exiting the program\n");
         exit(EXIT_FAILURE);
     }
  
     printf("before critical section %d\n", value);
 
-        if(sem_wait(semaphore) == -1)
+    if(sem_wait(semaphore) == -1)
     {
-        printf("Unbale to wait exiting the program\n");
+        printf("Unable to wait exiting the program\n");
         exit(EXIT_FAILURE);
     }
    
     if (sem_getvalue(semaphore, &value) == -1)
     {
-        printf("Unbale to Get semaphore value exiting the program\n");
+        printf("Unable to get semaphore value exiting the program\n");
         exit(EXIT_FAILURE);
     }
     
@@ -41,19 +54,24 @@ int main()
 
     if(sem_post(semaphore) == -1)
     {
-        printf("Unbale to release the Semaphore exiting the program\n");
+        printf("Unable to release the semaphore exiting the program\n");
         exit(EXIT_FAILURE);        
     }
 
     if (sem_getvalue(semaphore, &value) == -1)
     {
-        printf("Unbale to Get semaphore value exiting the program\n");
+        printf("Unable to get semaphore value exiting the program\n");
         exit(EXIT_FAILURE);
     }
     
     printf("After critical section %d\n", value);
 
-
-
     return 0;
 }
+
+/*
+Expected Output:
+before critical section 4
+In critical section 3
+After critical section 4
+*/
