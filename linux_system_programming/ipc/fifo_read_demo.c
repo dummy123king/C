d6a3488fbf4f5427993ca8d20628c94e824971cd
dummy123king <dummy123king@gmail.com>
2026-05-04 23:44:23 +0530

    added comments

--- linux_system_programming/ipc/fifo_read_demo.c
+++ linux_system_programming/ipc/fifo_read_demo.c
@@ -1,3 +1,8 @@
+/**
+ * @file fifo_read_demo.c
+ * @brief Demonstrates reading data from a FIFO (named pipe).
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,27 +10,45 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 
+/**
+ * @brief Main function to read data from a FIFO.
+ *
+ * Opens the FIFO "myFIFO" in read-only mode and continuously reads
+ * integers from it, printing them to the standard output.
+ *
+ * @param argc Number of command-line arguments.
+ * @param argv Array of command-line arguments.
+ * @return 0 on successful execution.
+ */
 int main(int argc, char **argv)
 {
-    int readData = 0;
+    int read_data = 0;
 
-    int readFd = open("myFIFO", O_RDONLY);
+    int read_fd = open("myFIFO", O_RDONLY);
     
-    if (readFd == -1)
+    if (read_fd == -1)
     {
-        printf("Unbale to open the FIFO in Read mode\n");
+        printf("Unable to open the FIFO in Read mode\n");
         exit(EXIT_FAILURE);
     }
     
     for (;;)
     {
-        if(read(readFd, &readData, sizeof(int)) == -1)
+        if(read(read_fd, &read_data, sizeof(int)) == -1)
         {
             printf("Unable to read data \n");
             exit(EXIT_FAILURE);
         }
-        printf("Read Data = %d\n", readData);
+        printf("Read Data = %d\n", read_data);
         sleep(2);
     }
     return 0;
 }
+
+/*
+Expected Output:
+Read Data = 0
+Read Data = 1
+Read Data = 2
+...
+*/
