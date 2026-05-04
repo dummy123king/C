6e7d5d1834b6a79ab1f682c1b440fb6c4a9d62e0
dummy123king <dummy123king@gmail.com>
2026-05-04 23:18:48 +0530

    added comments

--- sorting_techniques/insertion_sort.c
+++ sorting_techniques/insertion_sort.c
@@ -1,8 +1,19 @@
+/**
+ * @file insertion_sort.c
+ * @brief Demonstrates the insertion sort algorithm on an integer array.
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 
-void printArray(int *arr, int len)
+/**
+ * @brief Prints the elements of an integer array.
+ *
+ * @param arr Pointer to the integer array.
+ * @param len Length of the array.
+ */
+void print_array(int *arr, int len)
 {
     printf("[");
 
@@ -13,7 +24,13 @@ void printArray(int *arr, int len)
     printf("]\n");
 }
 
-void insertionSort(int *arr, int len)
+/**
+ * @brief Sorts an integer array using the insertion sort algorithm.
+ *
+ * @param arr Pointer to the integer array to be sorted.
+ * @param len Length of the array.
+ */
+void insertion_sort(int *arr, int len)
 {
     for (int i = 1; i < len; i++)
     {
@@ -28,13 +45,25 @@ void insertionSort(int *arr, int len)
     }
 }
 
-int main()
+/**
+ * @brief Entry point of the program.
+ *
+ * @return 0 on successful execution.
+ */
+int main(void)
 {
     int arr[] = {3, 3, 12, 5, 7, 0, 2};
     int len = sizeof(arr)/sizeof(arr[0]);
 
-    printArray(arr, len);
-    insertionSort(arr, len);
-    printArray(arr, len);
-
+    print_array(arr, len);
+    insertion_sort(arr, len);
+    print_array(arr, len);
+    
+    return 0;
 }
+
+/*
+Expected Output:
+[3, 3, 12, 5, 7, 0, 2, ]
+[0, 2, 3, 3, 5, 7, 12, ]
+*/
--- sorting_techniques/selection_sort.c
+++ sorting_techniques/selection_sort.c
@@ -1,8 +1,19 @@
+/**
+ * @file selection_sort.c
+ * @brief Demonstrates the selection sort algorithm on an integer array.
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 
-void printArray(int *arr, int len)
+/**
+ * @brief Prints the elements of an integer array.
+ *
+ * @param arr Pointer to the integer array.
+ * @param len Length of the array.
+ */
+void print_array(int *arr, int len)
 {
     printf("[");
     for (int i = 0; i < len; i++)
@@ -12,35 +23,54 @@ void printArray(int *arr, int len)
     printf("]\n");
 }
 
-void selectionSort(int *arr, int len)
+/**
+ * @brief Sorts an integer array using the selection sort algorithm.
+ *
+ * @param arr Pointer to the integer array to be sorted.
+ * @param len Length of the array.
+ */
+void selection_sort(int *arr, int len)
 {
-    int minIndex;
+    int min_index;
 
     for (int i = 0; i < len - 1; i++)
     {
-        minIndex = i;
+        min_index = i;
         for (int j = i + 1; j < len; j++)
         {
-            if (arr[j] < arr[minIndex])
+            if (arr[j] < arr[min_index])
             {
-                minIndex = j;
+                min_index = j;
             }
         }
-        if (minIndex != i)
+        if (min_index != i)
         {
-            int temp = arr[minIndex];
-            arr[minIndex] = arr[i];
+            int temp = arr[min_index];
+            arr[min_index] = arr[i];
             arr[i] = temp;
         }
     }
 }
 
-int main()
+/**
+ * @brief Entry point of the program.
+ *
+ * @return 0 on successful execution.
+ */
+int main(void)
 {
     int arr[] = {3, 3, 12, 5, 7, 0, 2};
 
     int len = sizeof(arr)/sizeof(arr[0]);
-    printArray(arr, len);
-    selectionSort(arr, len);
-    printArray(arr, len);
+    print_array(arr, len);
+    selection_sort(arr, len);
+    print_array(arr, len);
+    
+    return 0;
 }
+
+/*
+Expected Output:
+[3, 3, 12, 5, 7, 0, 2, ]
+[0, 2, 3, 3, 5, 7, 12, ]
+*/
