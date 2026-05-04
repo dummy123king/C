d14edd5e638f887da21ed1a885e90a24a6d8a98a
dummy123king <dummy123king@gmail.com>
2026-05-04 23:34:44 +0530

    added comments

--- arrays/one_dimensional_array.c
+++ arrays/one_dimensional_array.c
@@ -1,8 +1,18 @@
+/**
+ * @file one_dimensional_array.c
+ * @brief Demonstrates the initialization and printing of a 1D integer array.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-
-void printArray(int *ptr, int len)
+/**
+ * @brief Prints the elements of an integer array.
+ *
+ * @param ptr Pointer to the integer array.
+ * @param len Length of the array.
+ */
+void print_array(int *ptr, int len)
 {
     for (int i = 0; i < len; i++)
     {
@@ -11,15 +21,22 @@ void printArray(int *ptr, int len)
     printf("\n");
 }
 
+/**
+ * @brief Entry point of the program.
+ *
+ * @param argc Number of command-line arguments.
+ * @param argv Array of command-line arguments.
+ * @return 0 on successful execution.
+ */
 int main(int argc, char **argv)
 {
-    int intArray[10] = {1, 2, 5, 6, 7, 8, 9, 0, 88, 11};
-    int size = sizeof(intArray)/sizeof(intArray[0]);
-    printArray(intArray, size);
+    int int_array[10] = {1, 2, 5, 6, 7, 8, 9, 0, 88, 11};
+    int size = sizeof(int_array)/sizeof(int_array[0]);
+    print_array(int_array, size);
     return 0;
 }
 
-
 /*
-1, 2, 5, 6, 7, 8, 9, 0, 88, 11
+Expected Output:
+1, 2, 5, 6, 7, 8, 9, 0, 88, 11, 
 */
