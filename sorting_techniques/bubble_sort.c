84680183b5bde8b7014d1f6ced4df3fd6c81fe4b
dummy123king <dummy123king@gmail.com>
2026-05-04 23:15:45 +0530

    added comments

--- sorting_techniques/bubble_sort.c
+++ sorting_techniques/bubble_sort.c
@@ -1,7 +1,18 @@
+/**
+ * @file bubble_sort.c
+ * @brief Demonstrates the bubble sort algorithm on an integer array.
+ */
+
 #include <stdio.h>
 #include <stdbool.h>
 
-void printArray(int *arr, int len)
+/**
+ * @brief Prints the elements of an integer array.
+ *
+ * @param arr Pointer to the integer array.
+ * @param len Length of the array.
+ */
+void print_array(int *arr, int len)
 {
     printf("[");
     for (int i = 0; i < len; i++)
@@ -11,12 +22,19 @@ void printArray(int *arr, int len)
     printf("]\n");
 }
 
-void  bubbleSort(int *arr, int len)
+/**
+ * @brief Sorts an integer array using the bubble sort algorithm.
+ *
+ * @param arr Pointer to the integer array to be sorted.
+ * @param len Length of the array.
+ */
+void bubble_sort(int *arr, int len)
 {
-    bool isSwapped = false;
+    bool is_swapped;
 
     for (int i = 0; i < len - 1; i++)
     {
+        is_swapped = false;
         for (int j = 0; j < len - 1 -i; j++)
         {
             if (arr[j] > arr[j + 1])
@@ -24,23 +42,35 @@ void  bubbleSort(int *arr, int len)
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
-                isSwapped = true;
+                is_swapped = true;
             }
         }
-        if (isSwapped != true)
+        if (is_swapped != true)
         {
             break;
         }
     }        
 }
 
-int main()
+/**
+ * @brief Entry point of the program.
+ *
+ * @return 0 on successful execution.
+ */
+int main(void)
 {
     int arr[] = {3, 3, 12, 5, 7, 0, 2};
 
     int len = sizeof(arr)/sizeof(arr[0]);
-    printArray(arr, len);
-    bubbleSort(arr, len);
-    printArray(arr, len);
-
+    print_array(arr, len);
+    bubble_sort(arr, len);
+    print_array(arr, len);
+    
+    return 0;
 }
+
+/*
+Expected Output:
+[3, 3, 12, 5, 7, 0, 2, ]
+[0, 2, 3, 3, 5, 7, 12, ]
+*/
