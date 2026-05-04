a22078d7c7cc59efb4f7b424141f7f139c92fb2c
dummy123king <dummy123king@gmail.com>
2026-05-04 23:50:16 +0530

    added comments

--- linux_system_programming/ipc/pipe_communication.c
+++ linux_system_programming/ipc/pipe_communication.c
@@ -1,14 +1,29 @@
+/**
+ * @file pipe_communication.c
+ * @brief Demonstrates inter-process communication using a pipe between a parent and child process.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/fcntl.h>
 #include <string.h>
 
-int main(int atgc, char **argv)
+/**
+ * @brief Main function to create a pipe and facilitate communication.
+ *
+ * Creates a pipe, forks a child process, and has the child write to the pipe
+ * while the parent reads from it.
+ *
+ * @param argc Number of command-line arguments.
+ * @param argv Array of command-line arguments.
+ * @return 0 on successful execution.
+ */
+int main(int argc, char **argv)
 {
     int fd[2];
-    char writeBuf[] = "Hello from Write END";
-    char readBuf[100];
+    char write_buf[] = "Hello from Write END";
+    char read_buf[100];
     if (pipe(fd) != 0)
     {
         printf("Unable to create pipe\n");
@@ -24,7 +39,7 @@ int main(int atgc, char **argv)
             printf("Unable to close the Read end in child process\n");
             exit(EXIT_FAILURE);
         }
-        if (write(fd[1], writeBuf, strlen(writeBuf)) == -1)
+        if (write(fd[1], write_buf, strlen(write_buf)) == -1)
         {
             printf("Unable to write into the pipe\n");
             exit(EXIT_FAILURE);
@@ -39,21 +54,21 @@ int main(int atgc, char **argv)
     {
         printf("parent process\n");
 
-        int noOfBytesRead = 0;
+        int no_of_bytes_read = 0;
         if (close(fd[1]) == -1)
         {
             printf("Unable to close the Write end in Parent process\n");
         }
 
-        noOfBytesRead = read(fd[0], readBuf, strlen(writeBuf));
-        if (noOfBytesRead == -1)
+        no_of_bytes_read = read(fd[0], read_buf, strlen(write_buf));
+        if (no_of_bytes_read == -1)
         {
             printf("Unable to Read into the pipe\n");
             exit(EXIT_FAILURE);
         }
 
-        readBuf[noOfBytesRead] = '\0';
-        printf("Read Buffer: %s\n", readBuf);
+        read_buf[no_of_bytes_read] = '\0';
+        printf("Read Buffer: %s\n", read_buf);
         
         if (close(fd[0]) == -1)
         {
@@ -64,3 +79,10 @@ int main(int atgc, char **argv)
 
     return 0;
 }
+
+/*
+Expected Output (order of first two lines may vary):
+parent process
+Child process
+Read Buffer: Hello from Write END
+*/
